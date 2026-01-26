bda2e646880f6d388502dd95b5a52a5221e6a65c
Mr-Mhlongo <syaonmhlongo100@gmail.com>
2026-01-26 15:15:19 +0200

    login logic

--- src/store/authSlice.ts
+++ src/store/authSlice.ts
@@ -3,3 +3,15 @@ import axios from "axios";
 
 
 const API_URL = "http://localhost:3000";
+
+export const loginUser = createAsyncThunk(
+  "auth/login",
+  async (data: { email: string; password: string }, thunkAPI) => {
+    try {
+      const response = await axios.post(`${API_URL}/auth/login`, data);
+      return response.data; 
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.response?.data || "Login failed");
+    }
+  }
+);
