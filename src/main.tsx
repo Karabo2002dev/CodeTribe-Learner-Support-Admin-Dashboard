a1169ffe7c5c1857df6c00ceffe448e086cf5ca6
Mr-Mhlongo <syaonmhlongo100@gmail.com>
2026-01-26 15:47:28 +0200

    wrapped app with redux provider

--- src/main.tsx
+++ src/main.tsx
@@ -1,18 +1,21 @@
-import ReactDOM from 'react-dom/client';
-import {ThemeProvider} from '@mui/material/styles';
-import { CssBaseline } from '@mui/material';
-import {BrowserRouter}  from "react-router-dom"
-import theme from './theme.ts';
-import './index.css'
-import App from './App.tsx'
+import ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+
+import theme from "./theme";
+import "./index.css";
+import App from "./App";
+import { store } from "./store/store";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  
-  <BrowserRouter>
-   <ThemeProvider theme={theme}>
-      <CssBaseline />
-      <App />
-    </ThemeProvider>
-  </BrowserRouter>
-   
+  <Provider store={store}> 
+    <BrowserRouter>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
+    </BrowserRouter>
+  </Provider>
 );
