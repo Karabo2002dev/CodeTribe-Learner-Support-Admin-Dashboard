a2826fc924b4e06aa0a14e4099dde40c96fd60b5
Mr-Mhlongo <syaonmhlongo100@gmail.com>
2026-02-25 16:13:55 +0200

    updated local storage handling

--- src/pages/AssignedQueries.tsx
+++ src/pages/AssignedQueries.tsx
@@ -45,7 +45,6 @@ export default function AssignedQueries() {
   }, []);
 
   const handleLogout = () => {
-    localStorage.removeItem("token");
     localStorage.removeItem("user");
     navigate("/login");
   };
