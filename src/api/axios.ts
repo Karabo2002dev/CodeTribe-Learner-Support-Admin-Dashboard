119670d89574c0ddc807c65eda0eb81ed10dac74
Mr-Mhlongo <syaonmhlongo100@gmail.com>
2026-02-25 14:34:37 +0200

    Automatically attach token + Smooth API calls

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,13 +1,13 @@
 import axios from "axios";
 
-const API_URL = import.meta.env.VITE_API_URL
+const API_URL = import.meta.env.VITE_API_URL;
 
-const api = axios.create({
+const axiosInstance = axios.create({
   baseURL: API_URL,
 });
 
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("auth_token");
+axiosInstance.interceptors.request.use((config) => {
+  const token = localStorage.getItem("token");
 
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
@@ -16,4 +16,4 @@ api.interceptors.request.use((config) => {
   return config;
 });
 
-export default api;
+export default axiosInstance;
